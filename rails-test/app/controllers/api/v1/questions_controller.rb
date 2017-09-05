c86361a9dda1436da4dad0bddd05752692e21dd5
chetan <chetan@rentsetgo.co>
2017-09-05 11:57:09 +0530

    Validate API request of Tenant API key

--- rails-test/app/controllers/api/v1/questions_controller.rb
+++ rails-test/app/controllers/api/v1/questions_controller.rb
@@ -1,11 +1,23 @@
 class Api::V1::QuestionsController < ApiController
+	skip_before_filter  :verify_authenticity_token
+	before_action :validate_Api_key!
 	respond_to :html
-	layout "application"
+
 	def index
+		return head :forbidden unless has_valid_api_key?
 		@data = Question.all.to_json(:include => {:user => {:only => [:id, :name] }, 
                                 :answers => {:include => { :user => {:only => [:id, :name] } },
                                 :only => [:id, :body] } }, 
                                 :except => [:created_at, :updated_at, :user_id, :private])
-		# render json: @data
+		
 	end
+
+	private
+		def has_valid_api_key?
+			Tenant.pluck(:api_key).include? params[:api_key]
+		end
+
+		def validate_Api_key!
+			return head :forbidden unless has_valid_api_key?
+		end
 end
